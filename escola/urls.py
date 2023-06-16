c96dce8688fa28f2b65c4ff063a7699b4b958001
Andre Oliveira <dev.andrefc@gmail.com>
2023-06-16 15:11:59 -0300

    Criação da APIView de cursos e avaliações para o método HTTP GET

--- escola/urls.py
+++ escola/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('api/v1/', include('cursos.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
 ]
