02b99bbc50545e3188d6769d6a2af7bdd3f56e7e
Andre Oliveira <dev.andrefc@gmail.com>
2023-06-30 11:43:54 -0300

    Implementação do CRUD de cursos e avaliações com o generic views

---
+++ cursos/views1.py
@@ -0,0 +1,35 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,
+                        status=status.HTTP_201_CREATED)
+
+class AvaliacaoAPIView(APIView):
+    
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(date=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,
+                        status=status_201_CREATED)
