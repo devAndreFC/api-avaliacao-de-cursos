992caa53e7583bb2d48e6af1fb72d6f98c50fc94
Andre Oliveira <dev.andrefc@gmail.com>
2023-07-02 10:05:35 -0300

    Outras duas opções de relaçoes do serializer: HyperLinked e Primary Key related field

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -22,8 +22,18 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 
 class CursoSerializer(serializers.ModelSerializer):
-    # Nested relationshipp
-    avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+    # Nested relationshipp - usada para poucos dados, mostra todas as avaliações dos cursos na mesma tela
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+    
+    # HyperLinked Related Field - cria links das avalições dos cursos. Usada para quantidade média de dados
+    # avaliacoes = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     view_name='avaliacao-detail')
+
+    # Primary Key Related Fiel - usada para muitos dados. Mostra apenas o id das avaliações dos cursos
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Curso
         fields = (
