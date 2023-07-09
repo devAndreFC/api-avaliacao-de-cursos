7cccb9dc9e8cc0fc93bde65f9891e1e2fbb898a3
Andre Oliveira <dev.andrefc@gmail.com>
2023-07-08 21:59:35 -0300

    Adicionado limite n número de requisições com Throttling

--- escola/settings.py
+++ escola/settings.py
@@ -145,7 +145,15 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute', # second, day, month, year
+        'user': '10/minute'
+    }
 
 }
 
